43a5197eda97e0162404114b606023a579cf0c55
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-09 15:37:05 +0000

    5 new functions written and tested error free

---
+++ pet_shop.rb
@@ -0,0 +1,24 @@
+# Functions file to be tested by pet_shop_spec.rb
+
+def pet_shop_name(name)
+  name[:name]
+end
+
+def total_cash(name)
+  return name[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop,value)
+  shop[:admin][:total_cash] += value
+end
+
+def pets_sold(shop)
+  shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop,number)
+  shop[:admin][:pets_sold] += number
+end
+
+def stock_count()
+end
